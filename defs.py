b0aa6b942508ed4755d8fb9fd730afb83eed00ca
GiovanniDiMasi <74247812+GiovanniDiMasi@users.noreply.github.com>
2021-05-11 09:10:40 +0200

    Add files via upload

---
+++ defs.py
@@ -0,0 +1,8 @@
+API_KEY= '2489d0f3bbd25ab7a052fd85f51c51f7-c1e1ede69ff4cfd710332990813cc9c3'
+ACCOUNT_ID = "101-012-19158890-001"
+OANDA_URL =  "https://api-fxpractice.oanda.com/v3"
+#OANDAREALURL = "https://api-fxtrade.oanda.com"
+SECURE_HEADER ={
+    'Authorization': f'Bearer {API_KEY}',
+    'Content-Type': 'application/json'
+}
